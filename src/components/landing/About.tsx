0ce9e9aa135aacfac7fcde8b1e0a9c6ab990d2b9
Alexis Alcaraz <esenciaweb.ia@gmail.com>
2026-01-06 19:29:05 -0300

    Remove gradient overlay from About section small image

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -81,7 +81,6 @@ export function About() {
                             transition={{ duration: 0.8, delay: 0.3 }}
                             className="absolute -bottom-8 -left-8 lg:-bottom-12 lg:-left-12 w-48 h-48 lg:w-64 lg:h-64 overflow-hidden border-4 border-black shadow-2xl hidden lg:block"
                         >
-                            <div className="absolute inset-0 bg-gradient-to-br from-secondary/30 to-transparent z-10" />
                             <Image
                                 src="/images/hero-small-main.png"
                                 alt="Detalle de diseño"
